68873b5c1b68fe7662f04aa71e51c8e743376275
jean <jeancs.patricio@gmail.com>
2018-12-24 12:06:01 -0300

    WIP: Colocando botão de calcular.

--- core/voyage/admin.py
+++ core/voyage/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 from voyage.models import *
 
-admin.site.register(DocumentoViagem)
+
+@admin.register(DocumentoViagem)
+class DocumentoViagemAdmin(admin.ModelAdmin):
+    add_form_template = 'admin/documento_viagem/documento_viagem_add_form.html'
+
+
+admin.site.site_header = 'Site administrativo Voyage'
 admin.site.register(Trajeto)
 admin.site.register(Agente)
 admin.site.register(Coordenador)
 admin.site.register(TipoViagem)
 admin.site.register(Cidade)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
